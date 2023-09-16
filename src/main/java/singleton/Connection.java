914576dff69f9e4f6a93be17944bed5c468e4f1b
khalilrached <khalilrached058@gmail.com>
2023-09-16 18:57:01 +0100

    implementing Singleton design pattern.

---
+++ src/main/java/singleton/Connection.java
@@ -0,0 +1,59 @@
+package singleton;
+
+public class Connection {
+    private String url;
+    private String username;
+    private String password;
+
+    private static Connection instance;
+
+    private Connection() {
+        url = "";
+        username = "";
+        password = "";
+    }
+
+    private Connection(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public static Connection getInstance() {
+        return instance = (instance == null) ? new Connection() : instance;
+    }
+
+    public static Connection getInstance(String url, String username, String password) {
+        return instance = (instance == null) ? new Connection(url, username, password) : instance;
+    }
+
+    @Override
+    public String toString() {
+        return "Connection{" + "url=" + url + ", username=" + username + ", password=" + password + '}';
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/singleton/Test.java
@@ -0,0 +1,9 @@
+package singleton;
+
+public class Test {
+    public static void main(String[] args) {
+        Connection connection = Connection.getInstance("jdbc:mysql://localhost:3306/test", "root", "root");
+        connection = Connection.getInstance();
+        System.out.println(connection);
+    }
+}
